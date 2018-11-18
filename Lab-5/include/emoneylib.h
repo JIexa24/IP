ff34579edb944ec5cf2da15493c591a79e633490
Doshinsky <akter1997@gmail.com>
2018-11-18 11:51:40 +0700

    Deleted whole lab.. Again.. Yup, it's me:)

--- Lab-5/include/emoneylib.h
+++ Lab-5/include/emoneylib.h
@@ -12,29 +12,14 @@
 #include <sys/types.h>
 #include <stdbool.h>
 
-#include "../include/encryptolib.h"
-#include "../include/cryptolib.h"
-#include "../include/extralib.h"
-#include "../include/hashtab.h"
-
 #define MAXINT 512000
 
 struct data_BANK {
-    //секретные ключи
     unsigned long int P;
     unsigned long int Q;
     unsigned long int c[5];
-
-    //открытые ключи
     unsigned long int N;
     unsigned long int d[5];
 } BANK;
-
-long int generation_simple_number_in_limit(long int MAX_LIMIT);
-long int inversion(long int a_euclid, long int b_euclid);
-void initialization_data_key_BANK();
-long int hash_calculation(long int m);
-void operation_customer(long int n);
-void list_many(long int amount_customer);
 
 #endif
--- Lab-5/src/main.c
+++ Lab-5/src/main.c
@@ -13,18 +13,9 @@
 
 int main(int argc, char const *argv[])
 {
-  long int my_amount_many, amount_customer;
-  initialization_data_key_BANK();
-  printf("Введите сумму в банке\n");
-  scanf("%ld", &my_amount_many);
-  while (1) {
-    printf("Сумма платежа\n");
-    scanf("%ld", &amount_customer);
-    if (amount_customer <= my_amount_many && amount_customer > 0 ) {
-      my_amount_many -= amount_customer;
-      list_many(amount_customer);
-    } else break;
+  if (argc < 2) {
+    fprintf(stderr, "[ERROR] Not enough arguments!\n");
+    exit(EXIT_FAILURE);
   }
-
   return EXIT_SUCCESS;
 }
